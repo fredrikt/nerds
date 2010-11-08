546be9c54ac69d60a2ff84d503da07911c0bd5b4
Johan Lundberg <lundberg@nordu.net>
2010-11-08 14:21:39 +0100

    Added run.sh to juniper_conf producer.

---
+++ producers/juniper_conf/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+dir=$(dirname $0)
+
+# You need to provide a path to a config file with -C like
+# ./run.sh -C config.conf.
+# A template config file should have been provided.
+# Use ./run.sh -h for help.
+exec $dir/juniper_conf.py $*
