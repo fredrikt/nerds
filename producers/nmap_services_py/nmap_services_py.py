0c5041b47b226f2d68995311c6765d3b1af55793
Johan Lundberg <lundberg@nordu.net>
2012-12-14 17:25:00 +0100

    Initial commit.

---
+++ producers/nmap_services_py/nmap_services_py.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+import json
+import nmap
+
+# nmap_services.py
+#
+# Rewrite of nmap_services producer in Python for the NERDS project
+# (http://github.com/fredrikt/nerds/).
+#
+# If you have Python <2.7 you need to install argparse manually.
+
+def scan(target, nmap_arguments, output_arguments):
+    def callback_result(host, scan_result):
+        d = nerds_format(host, scan_result)
+        output(d, output_arguments['out_dir'], output_arguments['no_write'])
+
+    nma = nmap.PortScannerAsync()
+    nma.scan(hosts=target, arguments=nmap_arguments, callback=callback_result)
+    return nma
+
+def nerds_format(host, data):
+    host_data = data['scan'][host]
+    nerds_format = {
+        'host':{
+            'name': host_data['hostname'],
+            'version': 1,
+            'nmap_services_py': None
+        }
+    }
+    nmap_services_py = {
+        'addresses': [host],
+        'hostnames':[host_data['hostname']],
+        'os': {},
+        'services':{
+            host: {}
+        }
+    }
+    if host_data.has_key('uptime'):
+        nmap_services_py['uptime'] = host_data.uptime()
+    if host_data.has_key('tcp'):
+        nmap_services_py['services'][host]['tcp'] = host_data['tcp']
+    if host_data.has_key('udp'):
+        nmap_services_py['services'][host]['udp'] = host_data['udp']
+    if host_data.has_key('ip'):
+        nmap_services_py['services'][host]['ip'] = host_data['ip']
+    if host_data.has_key('sctp'):
+        nmap_services_py['services'][host]['sctp'] = host_data['sctp']
+    if host_data.has_key('osclass'):
+        nmap_services_py['os']['class'] = host_data['osclass'][0]
+    if host_data.has_key('osmatch'):
+        nmap_services_py['os']['match'] = host_data['osmatch'][0]
+    nerds_format['host']['nmap_services_py'] = nmap_services_py
+    return nerds_format
+
+def output(d, out_dir, no_write=False):
+    out = json.dumps(d, sort_keys=True, indent=4)
+    if no_write:
+        print out
+    else:
+        if out_dir[-1] != '/': # Pad with / if user provides a broken path
+            out_dir += '/'
+        try:
+            try:
+                #TODO: check if file exists, if so combine results
+                f = open('%s%s' % (out_dir, d['host']['name']), 'w')
+            except IOError:
+                os.mkdir(out_dir) # The directory to write in must exist
+                f = open('%s%s' % (out_dir, d['host']['name']), 'w')
+            f.write(out)
+            f.close()
+        except IOError as (errno, strerror):
+            print "I/O error({0}): {1}".format(errno, strerror)
+
+def main():
+    # User friendly usage output
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-O', nargs='?', default='./json/', help='Path to output directory.')
+    parser.add_argument('-N', action='store_true', default=False, help='Don\'t write output to disk.')
+    parser.add_argument('--verbose', '-v', action='store_true', default=False)
+    parser.add_argument(
+        '--list',
+        '-L',
+        type=argparse.FileType('r'),
+        help="File with addresses or networks."
+    )
+    parser.add_argument(
+        'target',
+        nargs='?',
+        help="Target address or network to scan"
+    )
+    args = parser.parse_args()
+    output_arguments = {
+        'out_dir': args.O,
+        'no_write': args.N
+    }
+    nmap_arguments = '-PE -sV -O --osscan-limit -F'
+    scanners = []
+    if args.target:
+        scanners.append(scan(args.target, nmap_arguments, output_arguments))
+    elif args.list:
+        for target in args.list:
+            scanners.append(scan(target, nmap_arguments, output_arguments))
+    # Wait for the scanners to finish
+    while scanners:
+        if args.verbose:
+            print("Scanning >>>")
+        for scanner in scanners:
+            if not scanner.still_scanning():
+                scanners.remove(scanner)
+
+if __name__ == '__main__':
+    main()
