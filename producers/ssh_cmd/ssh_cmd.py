ff36c6dfd9b4396fede60607b6ecb0c41d72c45f
Markus Krogh <markus@nordu.net>
2016-08-24 12:45:50 +0000

    Output results to files

--- producers/ssh_cmd/ssh_cmd.py
+++ producers/ssh_cmd/ssh_cmd.py
@@ -1,6 +1,6 @@
-import json
 from fabric.api import run, settings, env, quiet, hosts
 from cli import cli, load_config
+from file import template, merge_nerds_file, save_to_json
 import converters
 
 
@@ -11,68 +11,46 @@ def ssh_cmd(host, cmd, options={}):
     return output
 
 
-class DefaultConf(object):
-    def __init__(self, config):
-        self.config = config
-
-    def get(self, section, key, default=''):
-        if self.config.has_option(section, key):
-            return self.config.get(section, key)
-        else:
-            return default
-
-    def has_option(self, section, option):
-        return self.config.has_option(section, option)
-
-    def get_section(self, section):
-        return dict(self.config.items(section))
-
-    def sections(self):
-        return self.config.sections()
-
-def template(path, producer_name):
-    if path:
-        with open(path) as fp:
-            _template = json.load(fp) 
-    else:
-        _template = {producer_name: True}
-
-    return _template
-
 def handle_convert(host, lines, config, producer):
     converter = config.get('convert')
     producer_name = config.get('producer_name', producer)
+
     if converter in ('line_to_host'):
         _template = template(config.get('template'), producer_name)
         result = converters.list_to_hosts(lines, producer_name, _template)
     elif converter in ('to_list'):
         list_key = config.get('list_key', producer_name)
-        result = converters.to_list(host, lines, producer_name, list_key) 
+        result = [converters.to_list(host, lines, producer_name, list_key)]
     elif converter in ('split'):
         seperator = config.get('seperator', ',')
-        result = converters.split(host, producer_name, lines, seperator)
+        result = [converters.split(host, producer_name, lines, seperator)]
     elif converter in ('csv_lines', 'csv'):
         host_key = config.get('host_key', 'host_name')
         header = config.get('header').split()
         result = converters.csv(producer_name, header, lines, host_key)
+    else:
+        result = []
+
     return result
 
-
 def main(producers, config, args):
-    hosts = config.get('base_conf', 'hosts').split()
-    for host in hosts:
-        for producer in producers:
+    base_hosts = config.get('base_conf', 'hosts')
+    for producer in producers:
+        hosts = config.get(producer, 'hosts', base_hosts).split()
+        for host in hosts:
             producer_conf = config.get_section(producer)
             cmd = config.get(producer, 'cmd')
 
             lines = ssh_cmd(host, cmd).splitlines()
-            print handle_convert(host, lines, producer_conf, producer)
-            
+            result = handle_convert(host, lines, producer_conf, producer)
+            # output result
+            for nerds in result:
+                save_to_json(nerds, args.out)
 
 
 if __name__ == '__main__':
     args = cli()
-    config = DefaultConf(load_config(args.config))
+    config = load_config(args.config)
     producers = config.sections()
     if 'base_conf' in producers:
         producers.remove('base_conf')
